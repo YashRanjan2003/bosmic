a7073996c7d4664ec81f3b6fe32eb980a72cb24e
YashRanjan2003 <60296034+YashRanjan2003@users.noreply.github.com>
2021-09-24 11:28:28 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    app: {
+        px: 'p',
+        token: 'ODExOTkyNTEzODc0MzYyMzY5.YC6Q9Q.mxcTSrtGJu5blwoYGKnS-PeeDqM',
+        playing: ':/',
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        discordPlayer: {}
+    }
+};
